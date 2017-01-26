5c0b99c4023f1f6e108c22546123b603ee4f6b5d
Allison Reiss <areiss@ucsd.edu>
2017-01-26 10:31:19 -0800

    Files

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,39 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("I changed it");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+	});
+	$(".project").click(function(e) {
+		projectClick(e);
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+}
+
+function projectClick(e) {
+	//console.log("Project clicked");
+	// prevent the page from reloading
+	e.preventDefault();
+
+	// In an event handler, $(this) refers to the 
+	// object that triggered the event
+	//$(this).css("background-color", "#7fff00");
+	var containingProject = $(e.target).closest(".project");
+
+//	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
+	var description = $(containingProject).find(".project-description");
+
+	if (description.length == 0) {
+		containingProject.append("<div class='project-description'><p>Description of the project.</></div>");
+	}
+	else {
+		$(description).toggle();
+	}
+
+
 }
